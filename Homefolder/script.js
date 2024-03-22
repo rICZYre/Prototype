f97ffa21f4abc4c97db18811388b1fa17f3f5bff
rICZYre <160023803+rICZYre@users.noreply.github.com>
2024-03-22 13:17:48 +0800

    .

---
+++ Homefolder/script.js
@@ -0,0 +1,9 @@
+function openPopup() {
+  document.getElementById("loginPopup").style.display = "block";
+  document.getElementById("Body").style.display = "rgba(0, 0, 0, 0.5)";
+  
+}
+
+function closePopup() {
+  document.getElementById("loginPopup").style.display = "none";
+}
